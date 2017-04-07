043b5f76c118dd7071d5d8d7b834c22cf229d161
wangxiaoming <wath99520@gmail.com>
2017-04-07 22:59:33 +0800

    Python 过期库自动更新

--- jenkins-test/test-01.py
+++ jenkins-test/test-01.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+# 我们在写Python的相信大家会遇到过同样的错误，执行时报错"AttributeError: 'module' object has no attribute 'urlopen'"
+# 分析导致报错的原因，系统存在过期库，
+# pip list  #列出所有安装的库
+# pip list --outdated #列出所有过期的库
+# 提供自动更新程序如下：
 
-a = [1, 2, 3]
-b = a[:]
-print b
+import pip
+from subprocess import call
+
+for dist in pip.get_installed_distributions():
+    call("pip install --upgrade " + dist.project_name, shell=True)
