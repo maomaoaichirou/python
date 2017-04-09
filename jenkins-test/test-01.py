66ae7fa9d553170cd9cef31aeb649d66944b265f
wangxiaoming <wath99520@gmail.com>
2017-04-09 18:02:02 +0800

    python  库 批量更新

--- jenkins-test/test-01.py
+++ jenkins-test/test-01.py
@@ -5,9 +5,14 @@
 # pip list  #列出所有安装的库
 # pip list --outdated #列出所有过期的库
 # 提供自动更新程序如下：
-
+'''
 import pip
 from subprocess import call
 
 for dist in pip.get_installed_distributions():
     call("pip install --upgrade " + dist.project_name, shell=True)
+'''
+a = 2
+b = 3
+c = a**b
+print "6 - c 的值为：", c
